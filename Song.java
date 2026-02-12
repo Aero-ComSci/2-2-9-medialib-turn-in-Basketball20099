42be0da028aeb35470e1b033e8292ab633899fb2
Basketball20099 <157422433+Basketball20099@users.noreply.github.com>
2026-02-11 21:10:52 -0800

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,17 @@
+/*
+ * Activity 2.2.9
+ */
+public class Song
+{
+  private String title;
+
+  public Song(String title)
+  {
+    this.title = title;
+  }
+
+  public String toString()
+  {
+    return "\"" + title + "\"";
+  }
+}
